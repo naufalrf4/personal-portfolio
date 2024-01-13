1b1bf6835b29ce4017f0f14f1cdaefdde73a4543
NRF <contact.admin@naufalrf.me>
2024-01-14 00:28:03 +0700

    0.1.3

--- components/pages/AboutSection.tsx
+++ components/pages/AboutSection.tsx
@@ -9,6 +9,7 @@ import {
   GraduationCap,
   Calendar,
   Briefcase,
+  Users,
 } from "lucide-react";
 import DevImage from "../partials/Image";
 
@@ -56,17 +57,17 @@ const qualificationData = [
     ],
   },
   {
-    title: "experience",
+    title: "work",
     data: [
       {
-        Company: "PT Bank BTPN Tbk",
-        Qualification:
-          "Project-Based Internship: Fullstack Developer BTPN Syariah x Rakamin Academy",
+        company: "PT Bank BTPN Tbk",
+        qualification:
+          "PBI: Fullstack Developer BTPN Syariah x Rakamin Academy",
         date: "2022",
       },
       {
-        Company: "PT. Miftah Putra Mandiri",
-        Qualification: "Student Internship",
+        company: "PT. Miftah Putra Mandiri",
+        qualification: "Student Internship",
         date: "2021",
       },
     ],
@@ -75,18 +76,19 @@ const qualificationData = [
     title: "organizational",
     data: [
       {
-        Company: "Himavo Micro IT",
-        Qualification: "Webmaster Mentor",
-        date: "2022 - present",
-      },
-      {
-        Company: "Obscura Photography Club",
-        Qualification: "Staff",
+        company: "Obscura Photography Club",
+        qualification: "Staff",
         date: "2023 - present",
       },
       {
-        Company: "Agrimovie Filmaker Club",
-        Qualification: "Staff",
+        company: "Himavo Micro IT",
+        qualification: "Webmaster Mentor",
+        date: "2022 - present",
+      },
+
+      {
+        company: "Agrimovie Filmaker Club",
+        qualification: "Staff",
         date: "2022 - present",
       },
     ],
@@ -99,17 +101,133 @@ const skillData = [
     data: [
       {
         title: "Frontend",
-        subtitle:
-          "HTML, CSS, JavaScript, TypeScript, React, Next.js, Tailwind CSS, Bootstrap, Material UI",
+        data: [
+          {
+            name: "HTML",
+            href: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+            icon: "/images/icons/html.svg",
+          },
+          {
+            name: "CSS",
+            href: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+            icon: "/images/icons/css.svg",
+          },
+          {
+            name: "Javascript",
+            href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+            icon: "/images/icons/javascript.svg",
+          },
+          {
+            name: "Typescript",
+            href: "https://www.typescriptlang.org/",
+            icon: "/images/icons/typescript.svg",
+          },
+          {
+            name: "React",
+            href: "https://reactjs.org/",
+            icon: "/images/icons/react.svg",
+          },
+          {
+            name: "Next.js",
+            href: "https://nextjs.org/",
+            icon: "/images/icons/nextjs.svg",
+          },
+          {
+            name: "Tailwind CSS",
+            href: "https://tailwindcss.com/",
+            icon: "/images/icons/tailwindcss.svg",
+          },
+          {
+            name: "Bootstrap",
+            href: "https://getbootstrap.com/",
+            icon: "/images/icons/bootstrap.svg",
+          },
+          {
+            name: "Framer Motion",
+            href: "https://www.framer.com/motion/",
+            icon: "/images/icons/framermotion.svg",
+          },
+        ],
       },
       {
         title: "Backend",
-        subtitle: "Node.js, Express.js, MongoDB, MySQL, PostgreSQL, Firebase",
+        data: [
+          {
+            name: "Node.js",
+            href: "https://nodejs.org/en/",
+            icon: "/images/icons/nodejs.svg",
+          },
+          {
+            name: "Express.js",
+            href: "https://expressjs.com/",
+            icon: "/images/icons/expressjs.svg",
+          },
+          {
+            name: "MongoDB",
+            href: "https://www.mongodb.com/",
+            icon: "/images/icons/mongodb.svg",
+          },
+          {
+            name: "MySQL",
+            href: "https://www.mysql.com/",
+            icon: "/images/icons/mysql.svg",
+          },
+          {
+            name: "Firebase",
+            href: "https://firebase.google.com/",
+            icon: "/images/icons/firebase.svg",
+          },
+        ],
       },
       {
         title: "Others",
-        subtitle:
-          "Git, GitHub, Figma, Adobe XD, Adobe Photoshop, Adobe Illustrator, Adobe Premiere Pro, Adobe After Effects, Microsoft Office",
+        data: [
+          {
+            name: "Git",
+            href: "https://git-scm.com/",
+            icon: "/images/icons/git.svg",
+          },
+          {
+            name: "Github",
+            href: "https://github.com",
+            icon: "/images/icons/github.svg",
+          },
+          {
+            name: "Arduino",
+            href: "https://www.arduino.cc/",
+            icon: "/images/icons/arduino.svg",
+          },
+          {
+            name: "Linux",
+            href: "https://www.linux.org/",
+            icon: "/images/icons/linux.svg",
+          },
+          {
+            name: "Docker",
+            href: "https://www.docker.com/",
+            icon: "/images/icons/docker.svg",
+          },
+          {
+            name: "Figma",
+            href: "https://www.figma.com/",
+            icon: "/images/icons/figma.svg",
+          },
+          {
+            name: "Adobe Photoshop",
+            href: "https://www.adobe.com/en/products/photoshop",
+            icon: "/images/icons/photoshop.svg",
+          },
+          {
+            name: "Adobe Illustrator",
+            href: "https://www.adobe.com/en/products/illustrator",
+            icon: "/images/icons/illustrator.svg",
+          },
+          {
+            name: "Adobe Premiere Pro",
+            href: "https://www.adobe.com/en/products/premiere.html",
+            icon: "/images/icons/premierepro.svg",
+          },
+        ],
       },
     ],
   },
@@ -143,91 +261,128 @@ const AboutSection = () => {
                 <TabsTrigger value="skills">Skills</TabsTrigger>
               </TabsList>
               <TabsContent value="personal">
-                <div className="flex flex-col gap-4">
+                <h3 className="items-center text-xl font-semibold mb-8 text-center lg:text-left">
+                  Personal Information
+                </h3>
+                <div className="flex flex-col gap-2">
                   {infoData.map((item, index) => (
                     <div
                       key={index}
-                      className="flex flex-row gap-4 items-center"
+                      className="flex flex-row gap-2 items-center"
                     >
-                      <div className="w-8 h-8 flex flex-row items-center">
+                      <div className="w-8 h-8 flex flex-row items-center text-primary">
                         {item.icon}
                       </div>
-                      <p className="text-foreground">{item.text}</p>
+                      <p className="text-muted-foreground">{item.text}</p>
                     </div>
                   ))}
                 </div>
               </TabsContent>
               <TabsContent value="education">
-                <div className="flex flex-col gap-4">
+                <h3 className="text-xl font-semibold mb-8 text-center lg:text-left">
+                  My Education
+                </h3>
+
+                <div className="flex flex-col gap-y-8">
+                  <div className="flex gap-2 items-center text-primary">
+                    <GraduationCap size={24} />
+                    <h4 className="capitalize font-medium">Education Major</h4>
+                  </div>
                   {getData(qualificationData, "education").map(
-                    (item: any, index: number) => (
-                      <div
-                        key={index}
-                        className="flex flex-col gap-2 p-4 border-primary border-2 rounded-md"
-                      >
-                        <div className="flex items-center gap-4">
-                          <GraduationCap
-                            className="text-foreground"
-                            size={24}
-                          />{" "}
-                          <div>
-                            <h3 className="text-foreground font-semibold">
+                    (item: any, index: number) => {
+                      return (
+                        <div className="flex gap-x-8 group" key={index}>
+                          <div className="h-[128px] w-[2px] bg-border relative ml-2">
+                            <div className="w-[12px] h-[12px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[128px] transition-all duration-500"></div>
+                          </div>
+                          <div className="sm:min-w-[360px] w-[320px] lg:min-h-[128px] border rounded-lg border-primary p-4 flex flex-col gap-2">
+                            <div className="flex items-center gap-2 font-semibold text-md leading-6 mb-2">
                               {item.title}
-                            </h3>
-                            <p className="text-foreground">{item.subtitle}</p>
-                            <p className="text-foreground">{item.date}</p>
+                            </div>
+                            <div className="text-base leading-none text-muted-foreground mb-3">
+                              {item.subtitle}
+                            </div>
+                            <div className="capitalize text-sm font-medium">
+                              {item.date}
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    )
+                      );
+                    }
                   )}
                 </div>
               </TabsContent>
               <TabsContent value="experience">
-                <div className="flex flex-col gap-4">
-                  {getData(qualificationData, "experience").map(
-                    (item: any, index: number) => (
-                      <div
-                        key={index}
-                        className="flex flex-col gap-2 p-4 border-primary border-2 rounded-md"
-                      >
-                        <div className="flex items-center gap-4">
-                          <Briefcase className="text-foreground" size={24} />{" "}
-                          <div>
-                            <h3 className="text-foreground font-semibold">
-                              {item.Company}
-                            </h3>
-                            <p className="text-foreground">
-                              {item.Qualification}
-                            </p>
-                            <p className="text-foreground">{item.date}</p>
+                <h3 className="items-center text-xl font-semibold mb-8 text-center lg:text-left">
+                  My Experience
+                </h3>
+                <div className="grid md:grid-cols-2 gap-y-8">
+                  <div className="flex flex-col gap-y-8">
+                    <div className="flex gap-2 items-center text-primary">
+                      <Briefcase size={24} />
+                      <h4 className="capitalize font-medium">Work</h4>
+                    </div>
+
+                    {getData(qualificationData, "work").map(
+                      (item: any, index: number) => {
+                        return (
+                          <div className="flex gap-x-2 group" key={index}>
+                            <div className="h-[128px] w-[2px] bg-border relative ml-2">
+                              <div className="w-[12px] h-[12px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[128px] transition-all duration-500"></div>
+                            </div>
+                            <div className="rounded-lg p-4 flex flex-col gap-2">
+                              <div className="flex items-center gap-2 font-semibold text-md leading-6 mb-2">
+                                {item.company}
+                              </div>
+                              <div className="text-base leading-5 text-muted-foreground mb-3">
+                                {item.qualification}
+                              </div>
+                              <div className="capitalize text-sm font-medium">
+                                {item.date}
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                      </div>
-                    )
-                  )}
+                        );
+                      }
+                    )}
+                  </div>
+                  <div className="flex flex-col gap-y-8">
+                    <div className="flex gap-2 items-center text-primary">
+                      <Users size={24} />
+                      <h4 className="capitalize font-medium">Organizational</h4>
+                    </div>
+
+                    {getData(qualificationData, "organizational").map(
+                      (item: any, index: number) => {
+                        return (
+                          <div className="flex gap-x-2 group" key={index}>
+                            <div className="h-[128px] w-[2px] bg-border relative ml-2">
+                              <div className="w-[12px] h-[12px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[128px] transition-all duration-500"></div>
+                            </div>
+                            <div className=" rounded-lg  p-4 flex flex-col gap-2">
+                              <div className="flex items-center gap-2 font-semibold text-md leading-6 mb-2">
+                                {item.company}
+                              </div>
+                              <div className="text-base leading-none text-muted-foreground mb-3">
+                                {item.qualification}
+                              </div>
+                              <div className="capitalize text-sm font-medium">
+                                {item.date}
+                              </div>
+                            </div>
+                          </div>
+                        );
+                      }
+                    )}
+                  </div>
                 </div>
               </TabsContent>
               <TabsContent value="skills">
-                <div className="flex flex-col gap-4">
-                  {getData(skillData, "skills").map(
-                    (item: any, index: number) => (
-                      <div
-                        key={index}
-                        className="flex flex-col gap-2 p-4 border-primary border-2 rounded-md"
-                      >
-                        <div className="flex items-center gap-4">
-                          <Briefcase className="text-foreground" size={24} />{" "}
-                          <div>
-                            <h3 className="text-foreground font-semibold">
-                              {item.title}
-                            </h3>
-                            <p className="text-foreground">{item.subtitle}</p>
-                          </div>
-                        </div>
-                      </div>
-                    )
-                  )}
+                <div className="text-center lg:text-left">
+                  <h3 className="items-center text-xl font-semibold mb-8 text-center lg:text-left">
+                    Daily Tools
+                  </h3>
+                 
                 </div>
               </TabsContent>
             </Tabs>
