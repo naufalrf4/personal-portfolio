1110d272fb3a13389526dda1d8369bad27b4cfc6
naufalrf4 <naufalrf4@gmail.com>
2024-02-16 17:55:39 +0700

    fix dialog 2 mobile

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -38,7 +38,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed scrollbar-thin scrollbar-thumb-primary scrollbar-track-background left-[50%] top-[50%] z-50 grid min-w-[60vh] md:min-w-[100vh] max-h-[75vh] overflow-auto translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg",
+        "fixed scrollbar-thin scrollbar-thumb-primary scrollbar-track-background left-[50%] top-[50%] z-50 grid min-w-[45vh] md:min-w-[100vh] max-h-[85vh] overflow-auto translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg",
         className
       )}
       {...props}
